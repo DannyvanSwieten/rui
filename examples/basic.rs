b71018dcf86d2caec72da8fb75440b35149fe268
Stijn Frishert <stijnfrishert@gmail.com>
2022-10-12 20:07:53 +0200

    Basic example uses UIApplicationDelegate

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,5 +1,16 @@
-use rui::application::Application;
+use rui::application::{Application, ApplicationModel, UIApplicationDelegate};
+
+struct Model;
+
+impl ApplicationModel for Model {
+    type MessageType = ();
+
+    fn handle_message(&mut self, _: Self::MessageType) {}
+}
 
 fn main() {
-    println!("Hello, world!");
+    let application = Application::new("Basic Example");
+    let delegate = UIApplicationDelegate::new();
+
+    application.run(delegate, Model);
 }
