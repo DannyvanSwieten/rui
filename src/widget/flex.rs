2e9917c2fc8be8254d6c233724593aef9b5d8190
Stijn Frishert <stijnfrishert@gmail.com>
2022-10-17 10:05:43 +0200

    Flex::column() and Flex::row()

--- src/widget/flex.rs
+++ src/widget/flex.rs
@@ -5,14 +5,29 @@ use crate::{
     widget::{style::Theme, ChildSlot, Event, EventCtx, PaintCtx, Widget},
 };
 
-pub struct Row<State> {
+enum Direction {
+    Horizontal,
+    Vertical,
+}
+
+pub struct Flex<State> {
+    direction: Direction,
     children: Vec<ChildSlot<State>>,
     spacing: f32,
 }
 
-impl<State: AppState> Row<State> {
-    pub fn new() -> Self {
+impl<State: AppState> Flex<State> {
+    pub fn row() -> Self {
+        Self::new(Direction::Horizontal)
+    }
+
+    pub fn column() -> Self {
+        Self::new(Direction::Vertical)
+    }
+
+    fn new(direction: Direction) -> Self {
         Self {
+            direction,
             children: Vec::new(),
             spacing: 0.0,
         }
@@ -32,7 +47,7 @@ impl<State: AppState> Row<State> {
     }
 }
 
-impl<State: AppState> Widget<State> for Row<State> {
+impl<State: AppState> Widget<State> for Flex<State> {
     fn event(&mut self, event: &Event, ctx: &mut EventCtx<State>, state: &mut State) -> bool {
         for child in &mut self.children {
             if child.event(event, ctx, state) {
@@ -49,6 +64,7 @@ impl<State: AppState> Widget<State> for Row<State> {
 
         // Start without child constraints
         let child_constraints = BoxConstraints::new();
+
         let constrained_sizes: Vec<Size> = self
             .children
             .iter_mut()
@@ -66,9 +82,18 @@ impl<State: AppState> Widget<State> for Row<State> {
         let constrained_size =
             constrained_sizes
                 .iter()
-                .fold(Size::new(0.0, 0.0), |mut acc, child_size| {
-                    acc.width += child_size.width + self.spacing;
-                    acc.height = acc.height.max(child_size.height);
+                .fold(Size::default(), |mut acc, child_size| {
+                    match self.direction {
+                        Direction::Horizontal => {
+                            acc.width += child_size.width + self.spacing;
+                            acc.height = acc.height.max(child_size.height);
+                        }
+                        Direction::Vertical => {
+                            acc.width = acc.width.max(child_size.width);
+                            acc.height += child_size.height + self.spacing;
+                        }
+                    }
+
                     acc
                 });
 
@@ -78,24 +103,48 @@ impl<State: AppState> Widget<State> for Row<State> {
             .fold(0.0, |acc, child| acc + child.flex());
 
         if total_flex > 0.0 {
-            let width = constraints.max_width().unwrap();
-            let unconstraint_width = width - constrained_size.width;
-            let flex_factor = unconstraint_width / total_flex;
+            let total_spacing = (self.children.len() as f32 - 1.0) * self.spacing;
+
+            let flex_factor = match self.direction {
+                Direction::Horizontal => {
+                    let width = constraints.max_width().unwrap();
+                    let unconstraint_width = width - total_spacing - constrained_size.width;
+                    unconstraint_width / total_flex
+                }
+                Direction::Vertical => {
+                    let height = constraints.max_height().unwrap();
+                    let unconstraint_height = height - total_spacing - constrained_size.height;
+                    unconstraint_height / total_flex
+                }
+            };
+
             for child in &mut self.children {
                 if child.flex() != 0.0 {
-                    let child_constraints = BoxConstraints::new()
-                        .with_max_width(flex_factor * child.flex())
-                        .with_max_height(constraints.max_height().unwrap());
+                    let child_constraints = match self.direction {
+                        Direction::Horizontal => BoxConstraints::new_with_max(
+                            flex_factor * child.flex(),
+                            constraints.max_height().unwrap(),
+                        ),
+                        Direction::Vertical => BoxConstraints::new_with_max(
+                            constraints.max_width().unwrap(),
+                            flex_factor * child.flex(),
+                        ),
+                    };
+
                     let child_size = child.layout(&child_constraints, state);
                     child.set_size(&child_size);
                 }
             }
         }
 
-        let mut position = Point::new(0.0, 0.0);
+        let mut position = Point::default();
         for child in &mut self.children {
             child.set_position(&position);
-            position.x += child.size().width + self.spacing;
+
+            match self.direction {
+                Direction::Horizontal => position.x += child.size().width + self.spacing,
+                Direction::Vertical => position.y += child.size().height + self.spacing,
+            }
         }
 
         Size::new(
@@ -115,126 +164,8 @@ impl<State: AppState> Widget<State> for Row<State> {
     }
 }
 
-impl<State: AppState> Default for Row<State> {
+impl<State: AppState> Default for Flex<State> {
     fn default() -> Self {
-        Self::new()
-    }
-}
-
-pub struct Column<State> {
-    children: Vec<ChildSlot<State>>,
-    spacing: f32,
-}
-
-impl<State: AppState> Column<State> {
-    pub fn new() -> Self {
-        Self {
-            children: Vec::new(),
-            spacing: 0.0,
-        }
-    }
-
-    pub fn push<W>(mut self, child: W) -> Self
-    where
-        W: Widget<State> + 'static,
-    {
-        self.children.push(ChildSlot::new(child));
-        self
-    }
-
-    pub fn with_spacing(mut self, spacing: f32) -> Self {
-        self.spacing = spacing;
-        self
-    }
-}
-
-impl<State: AppState> Widget<State> for Column<State> {
-    fn event(&mut self, event: &Event, ctx: &mut EventCtx<State>, state: &mut State) -> bool {
-        for child in &mut self.children {
-            if child.event(event, ctx, state) {
-                return true;
-            }
-        }
-
-        false
-    }
-
-    fn layout(&mut self, constraints: &BoxConstraints, state: &State) -> Size {
-        // It needs constraints
-        assert!(constraints.has_max());
-
-        let total_spacing = (self.children.len() as f32 - 1.0) * self.spacing;
-
-        // Start with no constraints
-        let child_constraints = BoxConstraints::new();
-        let constrained_sizes: Vec<Size> = self
-            .children
-            .iter_mut()
-            .flat_map(|child| {
-                if child.flex() == 0.0 {
-                    let child_size = child.layout(&child_constraints, state);
-                    child.set_size(&child_size);
-                    Some(child_size)
-                } else {
-                    None
-                }
-            })
-            .collect();
-
-        let constrained_size =
-            constrained_sizes
-                .iter()
-                .fold(Size::new(0.0, 0.0), |mut acc, child_size| {
-                    acc.height += child_size.height + self.spacing;
-                    acc.width = acc.width.max(child_size.width);
-                    acc
-                });
-
-        let total_flex = self
-            .children
-            .iter()
-            .fold(0.0, |acc, child| acc + child.flex());
-
-        if total_flex > 0.0 {
-            let height = constraints.max_height().unwrap();
-            let unconstraint_height = height - total_spacing - constrained_size.height;
-            let flex_factor = unconstraint_height / total_flex;
-            for child in &mut self.children {
-                if child.flex() != 0.0 {
-                    let child_constraints = BoxConstraints::new()
-                        .with_max_height(flex_factor * child.flex())
-                        .with_max_width(constraints.max_width().unwrap());
-                    let child_size = child.layout(&child_constraints, state);
-                    child.set_size(&child_size);
-                }
-            }
-        }
-
-        let mut position = Point::new(0.0, 0.0);
-        for child in &mut self.children {
-            child.set_position(&position);
-            position.y += child.size().height + self.spacing;
-        }
-
-        Size::new(
-            constraints.max_width().unwrap(),
-            constraints.max_height().unwrap(),
-        )
-    }
-
-    fn paint(&self, theme: &Theme, ctx: &PaintCtx, canvas: &mut dyn Canvas2D, state: &State) {
-        for child in &self.children {
-            child.paint(theme, ctx, canvas, state)
-        }
-    }
-
-    fn flex(&self) -> f32 {
-        0.0
-    }
-}
-
-impl<State: AppState> Default for Column<State> {
-    fn default() -> Self {
-        Self::new()
+        Self::row()
     }
 }
